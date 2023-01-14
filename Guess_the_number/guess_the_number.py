857ea041aadb75c1531734cd8bbfa581fbdbc9d8
Harshita Bhambhani <97826462+harshita1611@users.noreply.github.com>
2023-01-14 14:52:48 +0530

    updated

--- Guess_the_number/guess_the_number.py
+++ Guess_the_number/guess_the_number.py
@@ -1,31 +1,45 @@
+logo = '''
+   ____                         _   _                                    _               
+  / ___|_   _  ___  ___ ___    | |_| |__   ___     _ __  _   _ _ __ ___ | |__   ___ _ __ 
+ | |  _| | | |/ _ \/ __/ __|   | __| '_ \ / _ \   | '_ \| | | | '_ ` _ \| '_ \ / _ \ '__|
+ | |_| | |_| |  __/\__ \__ \   | |_| | | |  __/   | | | | |_| | | | | | | |_) |  __/ |   
+  \____|\__,_|\___||___/___/    \__|_| |_|\___|   |_| |_|\__,_|_| |_| |_|_.__/ \___|_|   
+                                                                                         
+'''
+print(logo)
 import random
-correct = random.randrange(1,100)
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
-print(f"Pssst, the correct answer is {correct}")
+attempts_easy = 10
+attempts_hard = 5
+def check(guess , answer , turns ):
+    if(guess == answer):
+        print(f"You win!! The answer is {answer}")
+    elif guess>answer :
+        print("Too High , try a lower number")
+        return turns-1
+    elif guess < answer :
+        print("Too Low , try a higher number")
+        return turns-1
+def difficulty():
+    choice = input("Choose difficulty 'easy' or 'hard'")
+    if choice == "easy":
+        return attempts_easy
+    if choice == "hard" :
+        return attempts_hard
+def game() :
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    answer = random.randrange(1, 100)
+    print(f"answer is {answer}")
+    turns = difficulty()
+    guess = 0
+    while guess!=answer:
+        print(f"You get {turns} turns to guess")
+        guess = int(input("Make a guess : "))
+        turns=check(guess, answer, turns)
+        if turns==0 :
+            print("You loose because you are out of attempts")
+            return
+        elif guess!= answer :
+            print("Guess Again!")
 
-print("choose a level :easy or hard")
-choice = input()
-low =1 
-high = 100
-def binary_search (element):
-	mid = (low+high)/2
-	if element == mid :
-		print(f"element {element} found ")
-	elif  element<mid :
-		print("You , loose Try a lower number")
-	elif  element>mid :
-		print("You , loose Try a greater number")
-if choice=="easy" :
-	attempts=10
-	print("You get 10 attempts")
-	for i in range(1,10) :
-		print("Make a guess")
-		guess = int(input())
-		binary_search(guess)
-		
-if choice=="hard":
-	attempts =5
-	print("You get 5 attempts")
-
-
+game()
